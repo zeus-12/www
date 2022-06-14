a831b56f36c3f62ae50c0151c845fbb41b042d78
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-06-14 20:08:11 +0530

    more changees- about,added photo

--- components/About.jsx
+++ components/About.jsx
@@ -6,37 +6,40 @@ const About = () => {
       <p className="text-2xl">⚡️About Me</p>
 
       <div className="flex flex-col md:flex-row gap-4 mt-2">
-        <div className="text-neutral-400 flex flex-col gap-3 md:w-2/3">
+        <div className="text-neutral-400 flex flex-col gap-3 md:w-2/3 md:max-w-2xl">
           <p>
             Hey! I'm Vishnu Vinod, I've been close to a computer since an early
-            age, and been really passionate about it ever since. I’ve always
-            loved creating something cool, and where else than in the internet
-            itself.
-          </p>
-          <p>
-            I started out building websites using no-code tools back in 2014,
-            infact i still remember deploying my{" "}
-            <a className="text-blue-300" href="https://speedcubing3.weebly.com">
-              first website
+            age, and been really passionate about it ever since. I started out
+            building websites using no-code tools back in 2014, infact i still
+            remember deploying my{" "}
+            <a href="https://speedcubing3.weebly.com">first website</a> on{" "}
+            <a className="text-blue-300" href="https://www.weebly.com">
+              Weebly
             </a>{" "}
-            on Weebly ,though it was just a really basic one, it still got a
-            special place in my heart. And later on, I taught myself how to
-            code, fast-forward to today, I do programming in various languages
-            and technologies.
+            ,though it was just a really basic one, it still got a special place
+            in my heart.
           </p>
 
           <p>
-            I'm interested in building something awesome with code and automate
-            tasks with code, currently focused on Web & Mobile Development.
+            And later on, I taught myself how to code, fast-forward to today, I
+            do programming in various languages and technologies. I'm interested
+            in building something awesome with code and automate tasks with
+            code, currently focused on Web & Mobile Development.
           </p>
           <p>
-            When I'm not coding I play games with my friends, binge shows/movies
-            online, or if the weather's good, play basketball! 🏀 or sit down
-            for a good game of chess. I also love listening to music. I mainly
-            listen to Hip-Hop, Pop and Lofi songs
+            When I'm not coding I play games with my friends, binge
+            shows/movies, sit down for a good game of chess, or if the weather's
+            good, play basketball! 🏀 I also love listening to Hip-Hop, Pop &
+            Lofi music.
           </p>
         </div>
-        <Image layout="fixed" src="/me.png" alt="me" width={64} height={64} />
+
+        <div
+          className="rounded-lg flex flex-1"
+          style={{ width: "100%", position: "relative" }}
+        >
+          <Image src="/me.png" alt="me" layout="fill" objectFit="contain" />
+        </div>
       </div>
     </div>
   );
