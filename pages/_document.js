503a164b7ff98ab52ffbd48c8474eebd555ccac3
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-07-23 00:34:42 +0530

    Added smooth scroll, fixed bug on 404 page

---
+++ pages/_document.js
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+function Document() {
+  return (
+    <Html className="scroll-smooth">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
+
+export default Document;
