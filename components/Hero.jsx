e965eea2b472cc80f40790edcb8cc58a23a9b7a9
Vishnu <vishnu@Vishnus-MacBook-Pro.local>
2022-08-22 00:23:35 +0530

    fixed hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -7,12 +7,11 @@ import Image from "next/image";
 const Hero = () => {
   return (
     <div
-      className="flex flex-col lg:flex-row"
       style={{
         minHeight: `calc(100vh - 10rem)`,
       }}
     >
-      <div className="self-start">
+      <div>
         <SlideUpWhenVisible>
           <p className="text-cyan-400 text-lg font-medium md:text-3xl">
             👋 Hey there!, I'm-
@@ -20,7 +19,7 @@ const Hero = () => {
           <p className="text-7xl font-bold md:text-8xl lg:text-9xl block">
             Vishnu Vinod
           </p>
-          <p className="text-xl mt-4 font-medium sm:text-3xl sm:w-5/6 md:w-3/4 xl:w-2/3">
+          <p className="text-xl mt-4 font-medium sm:text-3xl sm:w-5/6 md:w-3/4 xl:w-2/3 max-w-xl">
             Software Engineer.
             <span className="text-neutral-400">
               {" "}
@@ -30,50 +29,49 @@ const Hero = () => {
           <p className="mt-8 text-sm text-neutral-400 md:text-lg font-light">
             🚀 Exploring oppurtunities & side projects.
           </p>
-          <div className="flex gap-2 mt-8">
-            <LinkBox title="Github" link="https://github.com/zeus-12">
-              <SiGithub className="fill-cyan-400" />
-            </LinkBox>
+          <div className="flex flex-col lg:flex-row justify-between items-start">
+            <div className="flex gap-2 mt-8 mb-4">
+              <LinkBox title="Github" link="https://github.com/zeus-12">
+                <SiGithub className="fill-cyan-400" />
+              </LinkBox>
 
-            <LinkBox
-              title="LinkedIn"
-              link="https://www.linkedin.com/in/vishnu-vinod-362ab2230/"
-            >
-              <SiLinkedin className="fill-cyan-400" />
-            </LinkBox>
+              <LinkBox
+                title="LinkedIn"
+                link="https://www.linkedin.com/in/vishnu-vinod-362ab2230/"
+              >
+                <SiLinkedin className="fill-cyan-400" />
+              </LinkBox>
 
-            <LinkBox title="Email" link="mailto:writetovishnuvinod@gmail.com">
-              <IoIosMail className="fill-cyan-400 h-5 w-5" />
-            </LinkBox>
+              <LinkBox title="Email" link="mailto:writetovishnuvinod@gmail.com">
+                <IoIosMail className="fill-cyan-400 h-5 w-5" />
+              </LinkBox>
+            </div>
+            <div className="hidden lg:flex">
+              <SlideUpWhenVisible>
+                <Image
+                  className="rounded-xl"
+                  width="500px"
+                  height="390px"
+                  src="/me.png"
+                  alt="Vishnu Vinod"
+                  priority
+                />
+              </SlideUpWhenVisible>
+            </div>
+            <div className="lg:hidden self-center">
+              <Image
+                className="rounded-xl"
+                layout=""
+                width="750px"
+                height="585px"
+                src="/me.png"
+                alt="Vishnu Vinod"
+                priority
+              />
+            </div>
           </div>
         </SlideUpWhenVisible>
-      </div>
-      <div className="rounded-xl self-end  mx-auto lg:mr-auto mt-4 lg:mt-0">
-        <SlideUpWhenVisible>
-          {/* <img className="rounded-xl" src="/me.png" alt="Vishnu Vinod" /> */}
-          <div className="hidden lg:flex">
-            <Image
-              className="rounded-xl"
-              width="500px"
-              height="390px"
-              src="/me.png"
-              alt="Vishnu Vinod"
-              priority
-            />
-          </div>
-
-          <div className="lg:hidden">
-            <Image
-              className="rounded-xl"
-              layout=""
-              width="750px"
-              height="585px"
-              src="/me.png"
-              alt="Vishnu Vinod"
-              priority
-            />
-          </div>
-        </SlideUpWhenVisible>
+        <SlideUpWhenVisible></SlideUpWhenVisible>
       </div>
     </div>
   );
