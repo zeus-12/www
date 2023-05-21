deb914adecd66ba12861654ebac448e859d9b003
Vishnu <vishnuvinod072@gmail.com>
2023-05-22 00:58:32 +0530

    more changes

--- src/components/SnippetCard.tsx
+++ src/components/SnippetCard.tsx
@@ -16,14 +16,16 @@ const SnippetCard: React.FC<SnippetCardProps> = ({
 }) => {
   return (
     <Link href={`/snippets/${slug}`}>
-      <div className='bg-gray-900 rounded-md p-4 w-full inline-flex h-full hover:scale-[102%] ease-in-out transition duration-200'>
+      <div className='bg-gray-900 rounded-md p-4 w-full inline-flex h-full hover:scale-[102%] ease-in-out transition duration-200 group'>
         {techStack?.map((stack) => (
           <Badge color={TECH_STACK[stack].color} variant='light' key={stack}>
             {TECH_STACK[stack].title}
           </Badge>
         ))}
         <div>
-          <p className='text-xl font-semibold'>{title}</p>
+          <p className='text-xl font-semibold group-hover:text-pink-400'>
+            {title}
+          </p>
           <p className='text-gray-400'>{description}</p>
         </div>
       </div>
