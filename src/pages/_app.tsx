1076f07b41f8316d236bdc2ae94b1a1b2ee0e070
Vishnu <vishnuvinod072@gmail.com>
2023-05-21 20:24:46 +0530

    add css for code tag, add prism-theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import '../styles/globals.css';
 import { MantineProvider } from '@mantine/core';
 import ScrollObserver from '@/utils/ScrollObserver';
 import { AppProps } from 'next/app';
+import 'prism-themes/themes/prism-night-owl.css';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
