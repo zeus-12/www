647ef05d39402734e4377fa0d085311c4dd02d1d
Vishnu <vishnuvinod072@gmail.com>
2023-05-22 00:46:44 +0530

    add tailwind typography

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 function Document() {
   return (
-    <Html className='scroll-smooth scroll-mt-6'>
+    <Html className='scroll-smooth scroll-mt-6 dark'>
       <Head />
       <body>
         <Main />
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,9 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
+  darkMode: 'class',
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
