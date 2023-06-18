dd44c29dfebc8bcc7b6a2f50cb05090283ddb6ff
Vishnu <vishnuvinod072@gmail.com>
2023-06-19 00:36:41 +0530

    update stylings

--- src/pages/now.tsx
+++ src/pages/now.tsx
@@ -1,5 +1,6 @@
 import Link from '@/components/Link';
 import Mdx from '@/components/Mdx';
+import SlideUpWhenVisible from '@/hooks/SlideUpWhenVisible';
 import { Other, allOthers } from 'contentlayer/generated';
 import {
   GetStaticPropsContext,
@@ -12,18 +13,20 @@ const Now: NextPage<InferGetStaticPropsType<typeof getStaticProps>> = ({
   md,
 }) => {
   return (
-    <div className='max-w-3xl mx-auto px-6 sm:px-8'>
-      <h1 className='text-4xl font-bold mt-24'>Now</h1>
+    <SlideUpWhenVisible>
+      <div className='max-w-3xl mx-auto px-6 sm:px-8'>
+        <h1 className='text-4xl font-bold mt-24'>Now</h1>
 
-      <h2 className='text-gray-400'>
-        Inspired by{' '}
-        <Link href='https://nownownow.com/about'>nownownow.com</Link>
-      </h2>
+        <h2 className='text-gray-400'>
+          Inspired by{' '}
+          <Link href='https://nownownow.com/about'>nownownow.com</Link>
+        </h2>
 
-      <div className='text-gray-300'>
-        <Mdx code={md?.body?.code} />
+        <div className='text-gray-300'>
+          <Mdx code={md?.body?.code} />
+        </div>
       </div>
-    </div>
+    </SlideUpWhenVisible>
   );
 };
 export default Now;
