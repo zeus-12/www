45e856e2d3d42c01b2e294d620ead4e9dfa03603
Vishnu <vishnuvinod072@gmail.com>
2023-08-15 01:57:21 +0530

    update colors, add dots.svg

--- src/components/SnippetCard.tsx
+++ src/components/SnippetCard.tsx
@@ -18,14 +18,16 @@ const SnippetCard: React.FC<SnippetCardProps> = ({
 }) => {
   return (
     <Link href={`/snippets/${slug}`}>
-      <div className='bg-gray-900 rounded-md p-4 w-full inline-flex h-full hover:scale-[102%] ease-in-out transition duration-200'>
+      {/* bg-[#021712] */}
+      <div className='bg-[#07273C] rounded-md p-4 w-full inline-flex h-full hover:scale-[102%] ease-in-out transition duration-200'>
         {techStack?.map((stack) => (
           <Badge color={TECH_STACK[stack].color} variant='light' key={stack}>
             {TECH_STACK[stack].title}
           </Badge>
         ))}
         <div>
-          <p className='text-xl font-semibold text-emerald-300'>{title}</p>
+          {/* text-[#46D279] */}
+          <p className='text-xl font-semibold text-cyan-400'>{title}</p>
           <p className={cn('text-gray-400', fontSans.className)}>
             {description}
           </p>
