f8994a2d1e597a28715f67eddd34cba69e6c8fc4
Vishnu <vishnuvinod072@gmail.com>
2023-08-21 00:34:55 +0530

    /work page

---
+++ src/pages/work/index.tsx
@@ -0,0 +1,24 @@
+import Seo from '@/components/Seo';
+import SlideUpWhenVisible from '@/hooks/SlideUpWhenVisible';
+
+const Work = () => {
+  return (
+    <>
+      <Seo
+        title='Projects'
+        description="A collection of projects that I've worked on."
+      />
+      <div className='pt-8 px-4 lg:px-24 lg:py-16'>
+        <SlideUpWhenVisible>
+          <p className='text-3xl lg:text-5xl lg:mb-2 font-semibold tracking-tight'>
+            Work Experience
+          </p>
+          <p className='text-gray-400'>
+            These are the professional engagements that I&apos;ve pursued.
+          </p>
+        </SlideUpWhenVisible>
+      </div>
+    </>
+  );
+};
+export default Work;
