ed52cfbdd7fc5d9e816dd4f9c0ca13540a548bd0
Vishnu <vishnuvinod072@gmail.com>
2023-08-24 14:55:11 +0530

    gimme that seo juiceee

---
+++ src/pages/robots.ts
@@ -0,0 +1,11 @@
+export default function robots() {
+  return {
+    rules: [
+      {
+        userAgent: '*',
+      },
+    ],
+    sitemap: 'https://vishnuu.me/sitemap.xml',
+    host: 'https://vishnuu.me',
+  };
+}
