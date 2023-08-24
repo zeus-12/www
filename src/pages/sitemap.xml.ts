53517f5fc9ff0e820c4b5f36a91b4beef7531dd8
Vishnu <vishnuvinod072@gmail.com>
2023-08-24 17:33:02 +0530

    sitemapss

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,15 +1,45 @@
 import { allSnippets } from 'contentlayer/generated';
 
-export default async function sitemap() {
-  const blogs = allSnippets.map((snippet) => ({
-    url: `https://vishnuu.me/snippets/${snippet.slug}`,
-    lastModified: snippet.date,
-  }));
+const URL = 'https://vishnuu.me';
 
-  const routes = ['', '/blog', '/projects', '/now'].map((route) => ({
-    url: `https://vishnuu.me${route}`,
-    lastModified: new Date().toISOString().split('T')[0],
-  }));
-
-  return [...routes, ...blogs];
+function generateSiteMap() {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>${URL}</loc>
+       <loc>${`${URL}/projects`}</loc>
+       <loc>${`${URL}/now`}</loc>
+       <loc>${`${URL}/snippets`}</loc>
+     </url>
+     ${allSnippets
+       .map((snippet) => {
+         return `
+       <url>
+           <loc>${`${URL}/snippets/${snippet.slug}`}</loc>
+       </url>
+     `;
+       })
+       .join('')}
+       
+    
+   </urlset>
+ `;
 }
+
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }: { res: any }) {
+  const sitemap = generateSiteMap();
+  res.setHeader('Content-Type', 'text/xml');
+  res.write(sitemap);
+  res.end();
+
+  return {
+    props: {},
+  };
+}
+
+export default SiteMap;
