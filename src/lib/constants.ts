af8f8533b497bfad4bc3c13bab83eac39b67836f
Vishnu <vishnuvinod072@gmail.com>
2023-09-26 18:41:14 +0530

    add shortdesc field

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -29,9 +29,11 @@ export const PROJECTS_DATA: {
   deployedLink?: string;
   imageSrc: string;
   isFeatured: boolean;
+  shortDescription: string;
 }[] = [
   {
     title: 'FMHY',
+    shortDescription: 'Wiki, Search, Guides for FMHY',
     description:
       'A full-stack website for a popular reddit community FMHY, including a comprehensive Wiki, Search and Guides. Built a service which periodically scrapes and cleans the data from their GitHub page.',
     techStack: [
@@ -47,6 +49,8 @@ export const PROJECTS_DATA: {
   },
   {
     title: 'Mentora',
+    shortDescription:
+      'A student-centered portal with features for asking questions, sharing notes, finding study buddies, and accessing detailed course information from Institute Department Websites.',
     description:
       'An Academic portal for students of the campus, where they could ask doubts, upload notes, apply for a study-buddy, along with a detailed course page scraped from the Institute Department Websites.',
     techStack: [TECH_STACK.NEXTJS, TECH_STACK.MONGODB],
@@ -57,6 +61,8 @@ export const PROJECTS_DATA: {
   },
   {
     title: 'Everhealth',
+    shortDescription:
+      'A gamified mobile app featuring streaks, levels, an AI health feed, leaderboards, and dark mode.',
     description:
       'Mobile app that motivates patients to adhere to treatment plans and manage chronic conditions through the use of gamification techniques, with streak & level system, Health Feed AI doctor, leaderboards, and darkmode.',
     techStack: [
@@ -71,19 +77,19 @@ export const PROJECTS_DATA: {
     imageSrc: '/projects/everhealth.jpeg',
     isFeatured: true,
   },
-  {
-    title: 'msngr',
-    description:
-      'Messenger clone built with Nextjs App dir, Pusher, Prisma. Implemented read receipts, user status, group, one-on-one messaging, message attachments, file sharing.',
-    techStack: [
-      TECH_STACK.NEXTJS,
-      TECH_STACK.PUSHER,
-      TECH_STACK.PRISMA,
-      TECH_STACK.POSTGRESQL,
-    ],
-    imageSrc: '/projects/msngr.png',
-    isFeatured: false,
-  },
+  // {
+  //   title: 'msngr',
+  //   description:
+  //     'Messenger clone built with Nextjs App dir, Pusher, Prisma. Implemented read receipts, user status, group, one-on-one messaging, message attachments, file sharing.',
+  //   techStack: [
+  //     TECH_STACK.NEXTJS,
+  //     TECH_STACK.PUSHER,
+  //     TECH_STACK.PRISMA,
+  //     TECH_STACK.POSTGRESQL,
+  //   ],
+  //   imageSrc: '/projects/msngr.png',
+  //   isFeatured: false,
+  // },
   // {
   //   title: 'Redora',
   //   description:
@@ -99,6 +105,8 @@ export const PROJECTS_DATA: {
   // },
   {
     title: 'Ping',
+    shortDescription:
+      'Security app for live location accessing, nearby camera access.',
     description:
       "Security app for IITM allowing users to send their location to their contacts in case of an emergency. Enabling the security to track the user's location, access and record nearby cameras.",
     techStack: [
@@ -114,6 +122,7 @@ export const PROJECTS_DATA: {
   },
   {
     title: 'Coursepanel',
+    shortDescription: 'Landing page for a startup we built.',
     description: 'Landing page for a startup we built.',
     techStack: [TECH_STACK.NEXTJS],
     deployedLink: 'https://coursepanel.in',
@@ -123,6 +132,7 @@ export const PROJECTS_DATA: {
 
   {
     title: 'JMT Shaastra',
+    shortDescription: "IITM Junior Make-A-Thon event's website.",
     description:
       'Developed a website for Junior Make-A-Thon event, part of Shaastra IITM from scratch, enabling for the smooth registration of over 3000 students from all over the country.',
     techStack: [TECH_STACK.NEXTJS, TECH_STACK.SHEETS_API],
@@ -133,8 +143,10 @@ export const PROJECTS_DATA: {
   },
   {
     title: 'Comportal',
+    shortDescription:
+      'Complaint portal app we built during a 24 hour hackathon.',
     description:
-      'Complaint portal app which could potentially replace the current institute complaint portal',
+      'Complaint portal app which could potentially replace the current institute complaint portal, build during a 24 hour hackathon.',
     techStack: [TECH_STACK.NEXTJS, TECH_STACK.MONGODB],
     githubLink: 'https://github.com/zeus-12/comportal',
     deployedLink: 'https://comportal.vercel.app',
@@ -143,6 +155,7 @@ export const PROJECTS_DATA: {
   },
   {
     title: 'Grievance Portal',
+    shortDescription: 'Grievance portal for an MP serving Idukki Constituency.',
     description:
       'Grievance portal for an MP serving Idukki Constituency, benefiting 12 Lakhs+ citizens with their needs.',
     techStack: [TECH_STACK.NEXTJS, TECH_STACK.EXPRESS, TECH_STACK.POSTGRESQL],
@@ -152,6 +165,7 @@ export const PROJECTS_DATA: {
   },
   {
     title: 'IITM Mess Menu',
+    shortDescription: 'IITM Mess Menu app, updates based on day, week number.',
     description:
       'Mess Menu app for students of IITM, which automatically updates based on day and week number.',
     techStack: [TECH_STACK.NEXTJS],
