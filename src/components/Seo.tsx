38e806ee91336b0befb66374fc1acf4679681b9f
Vishnu <vishnuvinod072@gmail.com>
2023-10-05 22:52:25 +0530

    update seo, add favicons

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -65,12 +65,12 @@ export default function Seo(props: SeoProps) {
         </>
       )}
 
-      <link rel='icon' href='/favicon.svg' />
+      <link rel='icon' href='/favicon.ico' />
 
       {/* Favicons */}
-      {/* {favicons.map((linkProps) => (
+      {favicons.map((linkProps) => (
         <link key={linkProps.href} {...linkProps} />
-      ))} */}
+      ))}
       <meta name='msapplication-TileColor' content='#000000' />
       {/* <meta
         name='msapplication-TileImage'
@@ -81,85 +81,85 @@ export default function Seo(props: SeoProps) {
   );
 }
 
-// type Favicons = {
-//   rel: string;
-//   href: string;
-//   sizes?: string;
-//   type?: string;
-// };
+type Favicons = {
+  rel: string;
+  href: string;
+  sizes?: string;
+  type?: string;
+};
 
-// const favicons: Array<Favicons> = [
-//   {
-//     rel: 'apple-touch-icon',
-//     sizes: '57x57',
-//     href: '/favicon/apple-icon-57x57.png',
-//   },
-//   {
-//     rel: 'apple-touch-icon',
-//     sizes: '60x60',
-//     href: '/favicon/apple-icon-60x60.png',
-//   },
-//   {
-//     rel: 'apple-touch-icon',
-//     sizes: '72x72',
-//     href: '/favicon/apple-icon-72x72.png',
-//   },
-//   {
-//     rel: 'apple-touch-icon',
-//     sizes: '76x76',
-//     href: '/favicon/apple-icon-76x76.png',
-//   },
-//   {
-//     rel: 'apple-touch-icon',
-//     sizes: '114x114',
-//     href: '/favicon/apple-icon-114x114.png',
-//   },
-//   {
-//     rel: 'apple-touch-icon',
-//     sizes: '120x120',
-//     href: '/favicon/apple-icon-120x120.png',
-//   },
-//   {
-//     rel: 'apple-touch-icon',
-//     sizes: '144x144',
-//     href: '/favicon/apple-icon-144x144.png',
-//   },
-//   {
-//     rel: 'apple-touch-icon',
-//     sizes: '152x152',
-//     href: '/favicon/apple-icon-152x152.png',
-//   },
-//   {
-//     rel: 'apple-touch-icon',
-//     sizes: '180x180',
-//     href: '/favicon/apple-icon-180x180.png',
-//   },
-//   {
-//     rel: 'icon',
-//     type: 'image/png',
-//     sizes: '192x192',
-//     href: '/favicon/android-icon-192x192.png',
-//   },
-//   {
-//     rel: 'icon',
-//     type: 'image/png',
-//     sizes: '32x32',
-//     href: '/favicon/favicon-32x32.png',
-//   },
-//   {
-//     rel: 'icon',
-//     type: 'image/png',
-//     sizes: '96x96',
-//     href: '/favicon/favicon-96x96.png',
-//   },
-//   {
-//     rel: 'icon',
-//     type: 'image/png',
-//     sizes: '16x16',
-//     href: '/favicon/favicon-16x16.png',
-//   },
-//   {
-//     rel: 'manifest',
-//     href: '/favicon/manifest.json',
-//   },
-// ];
+const favicons: Array<Favicons> = [
+  // {
+  //   rel: 'apple-touch-icon',
+  //   sizes: '57x57',
+  //   href: '/favicon/apple-icon-57x57.png',
+  // },
+  // {
+  //   rel: 'apple-touch-icon',
+  //   sizes: '60x60',
+  //   href: '/favicon/apple-icon-60x60.png',
+  // },
+  // {
+  //   rel: 'apple-touch-icon',
+  //   sizes: '72x72',
+  //   href: '/favicon/apple-icon-72x72.png',
+  // },
+  // {
+  //   rel: 'apple-touch-icon',
+  //   sizes: '76x76',
+  //   href: '/favicon/apple-icon-76x76.png',
+  // },
+  // {
+  //   rel: 'apple-touch-icon',
+  //   sizes: '114x114',
+  //   href: '/favicon/apple-icon-114x114.png',
+  // },
+  // {
+  //   rel: 'apple-touch-icon',
+  //   sizes: '120x120',
+  //   href: '/favicon/apple-icon-120x120.png',
+  // },
+  // {
+  //   rel: 'apple-touch-icon',
+  //   sizes: '144x144',
+  //   href: '/favicon/apple-icon-144x144.png',
+  // },
+  // {
+  //   rel: 'apple-touch-icon',
+  //   sizes: '152x152',
+  //   href: '/favicon/apple-icon-152x152.png',
+  // },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '180x180',
+    href: '/favicon/apple-icon.png',
+  },
+  // {
+  //   rel: 'icon',
+  //   type: 'image/png',
+  //   sizes: '192x192',
+  //   href: '/favicon/android-icon-192x192.png',
+  // },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/favicon/favicon-32x32.png',
+  },
+  // {
+  //   rel: 'icon',
+  //   type: 'image/png',
+  //   sizes: '96x96',
+  //   href: '/favicon/favicon-96x96.png',
+  // },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: '/favicon/favicon-16x16.png',
+  },
+  // {
+  //   rel: 'manifest',
+  //   href: '/favicon/manifest.json',
+  // },
+];
