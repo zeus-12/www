2e352c0cf53416962b87ec3e165d5db6cdf9b6e6
Vishnu <vishnuvinod072@gmail.com>
2023-10-05 22:54:06 +0530

    use import alias

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
-import '../styles/globals.css';
+import '@/styles/globals.css';
 import { MantineProvider } from '@mantine/core';
 import ScrollObserver from '@/lib/ScrollObserver';
 import { AppProps } from 'next/app';
 import 'prism-themes/themes/prism-night-owl.css';
 import Navbar from '@/components/Navbar';
 import Seo from '@/components/Seo';
-import PlausibleProvider from 'next-plausible';
-import { fontMono, fontSans } from '@/lib/fonts';
+// import PlausibleProvider from 'next-plausible';
+import { fontSans } from '@/lib/fonts';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
