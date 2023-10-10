9f95790a0f28f7ac71827380227f78667234d772
Vishnu <vishnuvinod072@gmail.com>
2023-10-10 23:40:47 +0530

    fix????

--- next.config.js
+++ next.config.js
@@ -5,14 +5,14 @@ const { withPlausibleProxy } = require('next-plausible');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: securityHeaders,
-      },
-    ];
-  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/(.*)',
+  //       headers: securityHeaders,
+  //     },
+  //   ];
+  // },
   async redirects() {
     return [
       {
@@ -74,46 +74,46 @@ const ContentSecurityPolicy = `
     font-src 'self';
 `;
 
-const securityHeaders = [
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-  {
-    key: 'Content-Security-Policy',
-    value: ContentSecurityPolicy.replace(/\n/g, ''),
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-  {
-    key: 'Referrer-Policy',
-    value: 'origin-when-cross-origin',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-  {
-    key: 'X-Frame-Options',
-    value: 'DENY',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-  {
-    key: 'X-Content-Type-Options',
-    value: 'nosniff',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-  {
-    key: 'X-DNS-Prefetch-Control',
-    value: 'on',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-  {
-    key: 'Strict-Transport-Security',
-    value: 'max-age=31536000; includeSubDomains; preload',
-  },
-  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-  {
-    key: 'Permissions-Policy',
-    value: 'camera=(), microphone=(), geolocation=()',
-  },
-];
+// const securityHeaders = [
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+//   {
+//     key: 'Content-Security-Policy',
+//     value: ContentSecurityPolicy.replace(/\n/g, ''),
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+//   {
+//     key: 'Referrer-Policy',
+//     value: 'origin-when-cross-origin',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+//   {
+//     key: 'X-Frame-Options',
+//     value: 'DENY',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+//   {
+//     key: 'X-Content-Type-Options',
+//     value: 'nosniff',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+//   {
+//     key: 'X-DNS-Prefetch-Control',
+//     value: 'on',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+//   {
+//     key: 'Strict-Transport-Security',
+//     value: 'max-age=31536000; includeSubDomains; preload',
+//   },
+//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+//   {
+//     key: 'Permissions-Policy',
+//     value: 'camera=(), microphone=(), geolocation=()',
+//   },
+// ];
 
 module.exports = module.exports = withPlausibleProxy({
   subdirectory: 'analytics',
   scriptName: 'script',
-  customDomain: 'https://a.bignutty.xyz',
+  customDomain: 'https://i-totally-love-easylist.swmg.top',
 })(withContentlayer(nextConfig));
