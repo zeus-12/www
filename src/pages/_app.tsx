8dff1fec2c9abd86d2700d96dafe009578435927
Vishnu <vishnuvinod072@gmail.com>
2023-10-10 22:48:06 +0530

    re-add plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,31 +5,33 @@ import { AppProps } from 'next/app';
 import 'prism-themes/themes/prism-night-owl.css';
 import Navbar from '@/components/Navbar';
 import Seo from '@/components/Seo';
-// import PlausibleProvider from 'next-plausible';
+import PlausibleProvider from 'next-plausible';
 import { fontSans } from '@/lib/fonts';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    // <PlausibleProvider
-    //   domain='vishnuu.me'
-    //   selfHosted={true}
-    //   customDomain='https://a.bignutty.xyz'
-    // >
-    <ScrollObserver>
-      <MantineProvider
-        theme={{
-          colorScheme: 'dark',
-        }}
-      >
-        <main className={fontSans.className}>
-          <Seo />
-          <Navbar />
-          <div className=''>
-            <Component {...pageProps} />
-          </div>
-        </main>
-      </MantineProvider>
-    </ScrollObserver>
-    // </PlausibleProvider>
+    <PlausibleProvider
+      domain='vishnuu.me'
+      selfHosted={true}
+      customDomain='https://i-totally-love-easylist.swmg.top'
+      trackOutboundLinks={true}
+      taggedEvents={true}
+    >
+      <ScrollObserver>
+        <MantineProvider
+          theme={{
+            colorScheme: 'dark',
+          }}
+        >
+          <main className={fontSans.className}>
+            <Seo />
+            <Navbar />
+            <div className=''>
+              <Component {...pageProps} />
+            </div>
+          </main>
+        </MantineProvider>
+      </ScrollObserver>
+    </PlausibleProvider>
   );
 }
