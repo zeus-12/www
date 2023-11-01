44c7eb9d4e082ddf48152256324b2434406c065e
Vishnu <vishnuvinod072@gmail.com>
2023-11-01 23:38:47 +0530

    update security headers, policies

--- next.config.js
+++ next.config.js
@@ -5,14 +5,14 @@ const { withContentlayer } = require('next-contentlayer');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // async headers() {
-  //   return [
-  //     {
-  //       source: '/(.*)',
-  //       headers: securityHeaders,
-  //     },
-  //   ];
-  // },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: securityHeaders,
+      },
+    ];
+  },
   async redirects() {
     return [
       {
@@ -64,45 +64,46 @@ const ContentSecurityPolicy = `
     font-src 'self';
 `;
 
-// const securityHeaders = [
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
-//   {
-//     key: 'Content-Security-Policy',
-//     value: ContentSecurityPolicy.replace(/\n/g, ''),
-//   },
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
-//   {
-//     key: 'Referrer-Policy',
-//     value: 'origin-when-cross-origin',
-//   },
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
-//   {
-//     key: 'X-Frame-Options',
-//     value: 'DENY',
-//   },
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
-//   {
-//     key: 'X-Content-Type-Options',
-//     value: 'nosniff',
-//   },
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
-//   {
-//     key: 'X-DNS-Prefetch-Control',
-//     value: 'on',
-//   },
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
-//   {
-//     key: 'Strict-Transport-Security',
-//     value: 'max-age=31536000; includeSubDomains; preload',
-//   },
-//   // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
-//   {
-//     key: 'Permissions-Policy',
-//     value: 'camera=(), microphone=(), geolocation=()',
-//   },
-// ];
+const securityHeaders = [
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+  {
+    key: 'Content-Security-Policy',
+    value: ContentSecurityPolicy.replace(/\n/g, ''),
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+  {
+    key: 'Referrer-Policy',
+    value: 'origin-when-cross-origin',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+  {
+    key: 'X-Frame-Options',
+    value: 'DENY',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+  {
+    key: 'X-Content-Type-Options',
+    value: 'nosniff',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+  {
+    key: 'X-DNS-Prefetch-Control',
+    value: 'on',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+  {
+    key: 'Strict-Transport-Security',
+    value: 'max-age=31536000; includeSubDomains; preload',
+  },
+  // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+  {
+    key: 'Permissions-Policy',
+    value: 'camera=(), microphone=(), geolocation=()',
+  },
+];
 
 module.exports = withContentlayer(nextConfig);
+
 // = withPlausibleProxy({
 //   customDomain: 'https://i-totally-love-easylist.swmg.top',
 // })(
