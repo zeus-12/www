8b73b487f457ca97b80cd661b13d347fb37f4ae6
Vishnu <vishnuvinod072@gmail.com>
2024-02-04 13:40:59 +0530

    layout changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   return (
     <>
       <NextSeo />
-      <div className='py-4 mt-6 md:py-8 lg:pt-16'>
+      <div className='py-4 mt-6 md:py-8 lg:pt-8'>
         <div className='px-4 sm:mx-4 lg:px-12 md:px-8 xl:px-24 xl:pt-10'>
           <Hero />
           <AboutMe />
@@ -20,30 +20,27 @@ export default function Home() {
   );
 }
 // put this somewhere
-{
-  /* <p>
-              I've been close to a computer since an early age, and been
-              passionate about it ever since. I started out building websites
-              using no-code tools back in 2012, infact i still remember
-              deploying my{" "}
-              <a
-                className="text-blue-300"
-                href="https://speedcubing3.weebly.com"
-              >
-                first website
-              </a>{" "}
-              on{" "}
-              <a className="text-blue-300" href="https://www.weebly.com">
-                Weebly
-              </a>{" "}
-              And later on, I taught myself how to code,
-fast-forward to today,
-            </p>
 
-            <p>
-              When I'm not coding I play games with my friends, binge
-              shows/movies, sit down for a good game of chess, or if the
-              weather's good, play basketball! 🏀 I also love listening to
-              Hip-Hop, Pop & Lo-Fi music.
-            </p> */
-}
+//               I've been close to a computer since an early age, and been
+//               passionate about it ever since. I started out building websites
+//               using no-code tools back in 2012, infact i still remember
+//               deploying my{" "}
+//               <a
+//                 className="text-blue-300"
+//                 href="https://speedcubing3.weebly.com"
+//               >
+//                 first website
+//               </a>{" "}
+//               on{" "}
+//               <a className="text-blue-300" href="https://www.weebly.com">
+//                 Weebly
+//               </a>{" "}
+//               And later on, I taught myself how to code,
+// fast-forward to today,
+//             </p>
+
+//             <p>
+//               When I'm not coding I play games with my friends, binge
+//               shows/movies, sit down for a good game of chess, or if the
+//               weather's good, play basketball! 🏀 I also love listening to
+//               Hip-Hop, Pop & Lo-Fi music.
