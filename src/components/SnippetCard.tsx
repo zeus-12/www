2de75b986e2c49118e79a420e87b54450d5efd92
Vishnu <vishnuvinod072@gmail.com>
2024-02-22 21:20:20 +0530

    update snippets

--- src/components/SnippetCard.tsx
+++ src/components/SnippetCard.tsx
@@ -1,4 +1,4 @@
-import { Badge } from '@mantine/core';
+import { Badge, Card } from '@mantine/core';
 import { TECH_STACK } from '@/configs/projects';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
@@ -17,21 +17,20 @@ const SnippetCard: React.FC<SnippetCardProps> = ({
   slug,
 }) => {
   return (
-    <Link href={`/snippets/${slug}`}>
+    <Link
+      href={`/snippets/${slug}`}
+      className='bg-gray-900 border-[1px] border-gray-800 text-white hover:scale-[102%] transition-all duration-200 rounded-md px-2 py-4 md:px-4 md:py-6 w-full inline-flex h-full ease-in-out'
+    >
       {/* bg-[#021712] */}
-      <div className='bg-[#07273C] rounded-md p-4 w-full inline-flex h-full hover:scale-[102%] ease-in-out transition duration-200'>
-        {techStack?.map((stack) => (
-          <Badge color={TECH_STACK[stack].color} variant='light' key={stack}>
-            {TECH_STACK[stack].title}
-          </Badge>
-        ))}
-        <div>
-          {/* text-[#46D279] */}
-          <p className='text-xl font-semibold text-sky-200'>{title}</p>
-          <p className={cn('text-gray-400', fontSans.className)}>
-            {description}
-          </p>
-        </div>
+      {techStack?.map((stack) => (
+        <Badge color={TECH_STACK[stack].color} variant='light' key={stack}>
+          {TECH_STACK[stack].title}
+        </Badge>
+      ))}
+      <div>
+        {/* text-[#46D279] */}
+        <p className='text-xl font-semibold text-sky-200'>{title}</p>
+        <p className={cn('text-gray-400', fontSans.className)}>{description}</p>
       </div>
     </Link>
   );
