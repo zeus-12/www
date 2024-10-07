a2e69d66ee7b17b001b7b2243ba2c7e95e2e0eed
Vishnu <vishnuvinod072@gmail.com>
2024-10-07 10:37:32 +0530

    use primary variable for colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,20 @@
 
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx}',
-    './src/components/**/*.{js,ts,jsx,tsx}',
-    './src/content/**/*.{md,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/content/**/*.{md,mdx}",
   ],
-  darkMode: 'class',
+  darkMode: "class",
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#22D3EE",
+      },
+      backgroundColor: {
+        primary: "#22D3EE",
+      },
+    },
   },
   future: {
     hoverOnlyWhenSupported: true,
