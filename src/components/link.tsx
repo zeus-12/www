07c21ac29a684237085fc88b351bc83ca11ad65c
Vishnu <vishnuvinod072@gmail.com>
2024-10-15 21:27:25 +0530

    add space above underline

--- src/components/link.tsx
+++ src/components/link.tsx
@@ -26,7 +26,7 @@ const Link = ({
         className={cn(
           monochrome
             ? "hover:text-gray-400"
-            : "hover:text-black focus:text-cyan active:text-cyan hover:bg-cyan underline decoration-cyan transition-colors duration-150 ease-in-out",
+            : "hover:text-black focus:text-cyan active:text-cyan hover:bg-cyan underline decoration-cyan underline-offset-4 transition-colors duration-150 ease-in-out",
           className
         )}
         {...rest}
@@ -40,7 +40,7 @@ const Link = ({
       className={cn(
         monochrome
           ? "hover:text-gray-400"
-          : "hover:text-black focus:text-cyan active:text-cyan hover:bg-cyan underline decoration-cyan transition-colors duration-150 ease-in-out",
+          : "hover:text-black focus:text-cyan active:text-cyan hover:bg-cyan underline decoration-cyan underline-offset-4 transition-colors duration-150 ease-in-out",
         className
       )}
       target="_blank"
