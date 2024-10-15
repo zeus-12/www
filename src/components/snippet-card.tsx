ea93fe9a02df84fe5f230fb014e77550da8d53f9
Vishnu <vishnuvinod072@gmail.com>
2024-10-15 19:13:41 +0530

    fix badges

--- src/components/snippet-card.tsx
+++ src/components/snippet-card.tsx
@@ -1,8 +1,9 @@
-import { Badge, Card } from "@mantine/core";
 import { TECH_STACK } from "@/lib/projects";
 import Link from "next/link";
 import { cn } from "@/lib/utils";
 import { fontSans } from "@/lib/fonts";
+import { Badge } from "@/components/ui/badge";
+
 interface SnippetCardProps {
   techStack?: string[];
   title: string;
@@ -21,14 +22,20 @@ const SnippetCard: React.FC<SnippetCardProps> = ({
       href={`/snippets/${slug}`}
       className="bg-gray-900 border-[1px] border-gray-800 text-white hover:scale-[102%] transition-all duration-200 rounded-md px-2 py-4 md:px-4 md:py-6 w-full inline-flex h-full ease-in-out"
     >
-      {/* bg-[#021712] */}
       {techStack?.map((stack) => (
-        <Badge color={TECH_STACK[stack].color} variant="light" key={stack}>
+        <Badge
+          variant="secondary"
+          className="text-[--color] "
+          style={{
+            // @ts-ignore
+            "--color": TECH_STACK[stack].color,
+          }}
+          key={stack}
+        >
           {TECH_STACK[stack].title}
         </Badge>
       ))}
       <div>
-        {/* text-[#46D279] */}
         <p className="text-xl font-semibold text-sky-200">{title}</p>
         <p className={cn("text-gray-400", fontSans.className)}>{description}</p>
       </div>
