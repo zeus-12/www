128c9b17e015c9a6abd424dcd3e812850f256e14
vishnu <vishnuvinod072@gmail.com>
2025-07-19 01:13:23 +0530

    use geist

--- tailwind.config.js
+++ tailwind.config.js
@@ -60,6 +60,10 @@ module.exports = {
         md: "calc(var(--radius) - 2px)",
         sm: "calc(var(--radius) - 4px)",
       },
+      fontFamily: {
+        sans: ["var(--font-geist-sans)"],
+        mono: ["var(--font-geist-mono)"],
+      },
     },
   },
   future: {
