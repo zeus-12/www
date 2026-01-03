fdd3d4d6b7e05593e51188aa070b3a7560607a27
vishnu <vishnuvinod072@gmail.com>
2026-01-03 13:41:04 +0530

    fix

--- src/components/ui/spotlight-border.tsx
+++ src/components/ui/spotlight-border.tsx
@@ -9,14 +9,14 @@ export function SpotlightBorder({
   className?: string;
 }) {
   return (
-    <div className="relative overflow-hidden rounded-xl p-[1px] bg-zinc-700/30">
+    <div className="relative overflow-hidden rounded-xl bg-zinc-300/30 p-[1px] dark:bg-zinc-700/30">
       <Spotlight
         className="from-blue-600 via-blue-500 to-blue-400 blur-3xl dark:from-blue-200 dark:via-blue-300 dark:to-blue-400"
         size={124}
       />
       <div
         className={cn(
-          "relative h-full w-full rounded-xl",
+          "relative h-full w-full rounded-xl dark:bg-neutral-950",
           className,
         )}
       >
